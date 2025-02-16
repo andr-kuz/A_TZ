b986997c2087cca91565f14af8d03f8fce589e96
Andrei Kuznetsov <awakum192@gmail.com>
2025-02-16 18:42:22 +0300

    wip

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from typing import Generator
 
 def mk_graph(array):
     graph = defaultdict(list)
@@ -36,15 +36,43 @@ def return_grouped(array):
     return result
 
 
-if __name__ == '__main__':
-    input_list = []
-
-    with open("lng1.txt", "r") as file:
+def split_file_newlines(file_name: str) -> list[str]:
+    lines = []
+    with open(file_name, 'r') as file:
         lines = file.read().splitlines()
-        for line in lines:
-            input_list.append(line.split(";"))
-    graph = mk_graph(input_list)
-    res = return_grouped(input_list)
+    return lines
 
-    for i in range(len(res)):
-        print(f"Group {i+1}: {res[i]}")
+def map_values(lines: list[str]) -> list[list[str]]:
+    map = []
+    for line_i, line in enumerate(lines):
+        map.append([])
+        for value_i, value in enumerate(line.split(';')):
+            map[line_i].append(f'{value_i}:{value}')
+    return map
+
+
+def find_needle(needle: str, map: list[list[str]]) -> Generator[int, None, None]:
+    for line_i, line in enumerate(map):
+        if needle in line:
+            yield line_i
+            continue
+
+
+def group_values(split_file_content: list[str], map: list[list[str]]):
+    groups = {}
+    for group_n, line in enumerate(map):
+        groups[group_n] = []
+        for value in line:
+            for line_i in find_needle(value, map):
+                groups[group_n].append(split_file_content[line_i])
+    return groups
+
+
+if __name__ == '__main__':
+    newlines_split = split_file_newlines('lng.txt')
+    mapped_values = map_values(newlines_split)
+    grouped_values = group_values(newlines_split, mapped_values)
+    for group_n, values in grouped_values.items():
+        print(f'Group {group_n + 1}')
+        for value in values:
+            print(f'  {value}')
